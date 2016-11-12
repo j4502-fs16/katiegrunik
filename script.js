2bb701586944a2e704f199cc2e44e2b1a6d1cd36
katiegrunik <krgyv6@mail.missouri.edu>
2016-11-12 15:22:39 -0600

    changed javascript

--- script.js
+++ script.js
@@ -8,38 +8,3 @@ $(document).ready(function() {
 	 }
 	});
 });
-
-var newLink = document.createElement( "a" );
-
-var allParagraphs = document.getElementsByTagName('p');
-
-var firstParagraph = allParagraphs[0];
-
-function revealCopy(e) {
-    var allParagraphs = document.getElementsByTagName("p");
-    for( var i = 0; i < allParagraphs.length; i++ ) {
-        if( i === 0 ) {
-            continue;
-    }
-    allParagraphs[ i ].style.display = "block";
-  }
-  this.remove();
-  e.preventDefault();
-}
-
-newLink.setAttribute("href","#");
-
-newLink.innerHTML="Read More";
-
-newLink.setAttribute( "class", "more-link" );
-
-firstParagraph.appendChild( newLink );
-
-newLink.addEventListener( "click", revealCopy );
-
-for( var i = 0; i < allParagraphs.length; i++ ) {
-        if( i === 0 ) {
-        continue;
-      }
-    allParagraphs[ i ].style.display = "none";
-}
